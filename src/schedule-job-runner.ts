1efb57185f86140ad5a3f9fa5736c1a4b8984d93
David Dawson <me@daviddawson.me>
2022-01-09 11:49:46 +0000

    Improve schedule job runner interface

--- src/schedule-job-runner.ts
+++ src/schedule-job-runner.ts
@@ -1,16 +1,9 @@
 
 
 export interface ScheduleJobRunner {
-  addScheduledTask(component: string, name: string, config: {
-    isInterval: true
-    initialWaitMillis: number
-    interval: number
-  } | {
-    isInterval: false
-    timeout: number
-  }, data: any)
+  addScheduledTask(component: string, name: string, config: { isCron: true; crontab: string } | { isCron: false; timeout: number }, data: any): Promise<void>
 
-  removeSchedule(component: string, name: string)
+  removeSchedule(component: string, name: string): Promise<void>
 
-  addScheduleTaskListener(component: string, exec: (name: string, data: any) => Promise<void>)
+  addScheduleTaskListener(component: string, exec: (name: string, data: any) => Promise<void>): Promise<void>
 }
