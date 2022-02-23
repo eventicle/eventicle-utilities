b22f7e7ad87aff77eeb5b2d1c0505ae2fca927e3
David Dawson <me@daviddawson.me>
2022-02-23 21:20:49 +0000

    Add id to scheduler interface

--- src/schedule-job-runner.ts
+++ src/schedule-job-runner.ts
@@ -1,9 +1,9 @@
 
 
 export interface ScheduleJobRunner {
-  addScheduledTask(component: string, name: string, config: { isCron: true; crontab: string } | { isCron: false; timeout: number }, data: any): Promise<void>
+  addScheduledTask(component: string, name: string, id: string, config: { isCron: true; crontab: string } | { isCron: false; timeout: number }, data: any): Promise<void>
 
-  removeSchedule(component: string, name: string): Promise<void>
+  removeSchedule(component: string, name: string, id: string): Promise<void>
 
-  addScheduleTaskListener(component: string, exec: (name: string, data: any) => Promise<void>): Promise<void>
+  addScheduleTaskListener(component: string, exec: (name: string, id: string, data: any) => Promise<void>): Promise<void>
 }
